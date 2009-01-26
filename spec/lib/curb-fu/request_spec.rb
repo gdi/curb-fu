95828a8de37633db6ce908e602a2f62123653c73
Derek and Matt <drat@gdis-imac-2.local>
2009-01-26 10:41:47 -0500

    create_fields can now handle non-string and non-array data [#214]

--- spec/lib/curb-fu/request_spec.rb
+++ spec/lib/curb-fu/request_spec.rb
@@ -57,5 +57,18 @@ describe CurbFu::Request do
         {:host => "google.com", :port => 80, :path => "/search"},
         { 'q' => ['derek','matt'] })
     end
+    
+    it "should handle params that contain any non-Array or non-String data" do
+      @mock_curb = mock(Curl::Easy, :headers= => nil, :headers => {}, :response_code => 200, :body_str => 'yeeeah')
+      Curl::Easy.stub!(:new).and_return(@mock_curb)
+      @mock_q = Curl::PostField.content('q','1')
+      Curl::PostField.stub!(:content).with('q','1').and_return(@mock_q)
+      
+      @mock_curb.should_receive(:http_post).with(@mock_q)
+      
+      response = CurbFu::Request.post(
+        {:host => "google.com", :port => 80, :path => "/search"},
+        { 'q' => 1 })
+    end
   end
 end
