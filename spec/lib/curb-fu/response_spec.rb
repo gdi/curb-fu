30d916b3166bd58f1eadb83c67c23bbc961438a4
Derek Kastner <dkastner@gmail.com>
2009-01-28 16:43:48 -0500

    tests for 400 and 500 errors

--- spec/lib/curb-fu/response_spec.rb
+++ spec/lib/curb-fu/response_spec.rb
@@ -10,4 +10,16 @@ describe CurbFu::Response::Base do
     r.should be_a_kind_of(CurbFu::Response::OK)
     r.should_not be_a_kind_of(CurbFu::Response::Created)
   end
+  it "should create a 400 response" do
+    mock_curb = mock(Object, :response_code => 404, :body_str => 'OK')
+    r = CurbFu::Response::Base.create(mock_curb)
+    r.should be_a_kind_of(CurbFu::Response::Base)
+    r.should be_a_kind_of(CurbFu::Response::ClientError)
+  end
+  it "should create a 500 response" do
+    mock_curb = mock(Object, :response_code => 503, :body_str => 'OK')
+    r = CurbFu::Response::Base.create(mock_curb)
+    r.should be_a_kind_of(CurbFu::Response::Base)
+    r.should be_a_kind_of(CurbFu::Response::ServerError)
+  end
 end
