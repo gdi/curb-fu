03bd849024ba935f984a177e0b46e6bbdd086a13
Derek and Matt <drat@gdis-imac-2.local>
2009-02-03 16:36:20 -0500

    Set a timeout parameter for curb requests

--- spec/lib/curb-fu/response_spec.rb
+++ spec/lib/curb-fu/response_spec.rb
@@ -3,7 +3,7 @@ require 'htmlentities'
 
 describe CurbFu::Response::Base do
   it "should create a success response" do
-    mock_curb = mock(Object, :response_code => 200, :body_str => 'OK')
+    mock_curb = mock(Object, :response_code => 200, :body_str => 'OK', :headers => {}, :timeout= => nil)
     r = CurbFu::Response::Base.create(mock_curb)
     r.should be_a_kind_of(CurbFu::Response::Base)
     r.should be_a_kind_of(CurbFu::Response::Success)
@@ -11,13 +11,13 @@ describe CurbFu::Response::Base do
     r.should_not be_a_kind_of(CurbFu::Response::Created)
   end
   it "should create a 400 response" do
-    mock_curb = mock(Object, :response_code => 404, :body_str => 'OK')
+    mock_curb = mock(Object, :response_code => 404, :body_str => 'OK', :headers => {}, :timeout= => nil)
     r = CurbFu::Response::Base.create(mock_curb)
     r.should be_a_kind_of(CurbFu::Response::Base)
     r.should be_a_kind_of(CurbFu::Response::ClientError)
   end
   it "should create a 500 response" do
-    mock_curb = mock(Object, :response_code => 503, :body_str => 'OK')
+    mock_curb = mock(Object, :response_code => 503, :body_str => 'OK', :headers => {}, :timeout= => nil)
     r = CurbFu::Response::Base.create(mock_curb)
     r.should be_a_kind_of(CurbFu::Response::Base)
     r.should be_a_kind_of(CurbFu::Response::ServerError)
