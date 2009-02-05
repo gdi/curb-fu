0443a9fd298c700e8d4bd3e2d9e86056fbeb8968
Derek and Matt <drat@gdis-imac-2.local>
2009-02-05 14:49:50 -0500

    Made CurbFu Ruby 1.9.1 compatible. YAY!

--- lib/curb-fu/response.rb
+++ lib/curb-fu/response.rb
@@ -33,65 +33,65 @@ module CurbFu
         response = self.new(curb)
 
         case response.status
-        when 100..199:
+        when 100..199;
           response.extend CurbFu::Response::Information
           case response.status
-          when 101: response.extend CurbFu::Response::Continue
-          when 102: response.extend CurbFu::Response::SwitchProtocl
+          when 101; response.extend CurbFu::Response::Continue
+          when 102; response.extend CurbFu::Response::SwitchProtocl
           end
-        when 200..299:
+        when 200..299;
           response.extend CurbFu::Response::Success
           case response.status
-          when 200: response.extend CurbFu::Response::OK
-          when 201: response.extend CurbFu::Response::Created
-          when 202: response.extend CurbFu::Response::Accepted
-          when 203: response.extend CurbFu::Response::NonAuthoritativeInformation
-          when 204: response.extend CurbFu::Response::NoContent
-          when 205: response.extend CurbFu::Response::ResetContent
-          when 206: response.extend CurbFu::Response::PartialContent
+          when 200; response.extend CurbFu::Response::OK
+          when 201; response.extend CurbFu::Response::Created
+          when 202; response.extend CurbFu::Response::Accepted
+          when 203; response.extend CurbFu::Response::NonAuthoritativeInformation
+          when 204; response.extend CurbFu::Response::NoContent
+          when 205; response.extend CurbFu::Response::ResetContent
+          when 206; response.extend CurbFu::Response::PartialContent
           end
-        when 300..399:
+        when 300..399;
           response.extend CurbFu::Response::Redirection
           case response.status
-          when 300: response.extend CurbFu::Response::MultipleChoice
-          when 301: response.extend CurbFu::Response::MovedPermanently
-          when 302: response.extend CurbFu::Response::Found
-          when 303: response.extend CurbFu::Response::SeeOther
-          when 304: response.extend CurbFu::Response::NotModified
-          when 305: response.extend CurbFu::Response::UseProxy
-          when 307: response.extend CurbFu::Response::TemporaryRedirect
+          when 300; response.extend CurbFu::Response::MultipleChoice
+          when 301; response.extend CurbFu::Response::MovedPermanently
+          when 302; response.extend CurbFu::Response::Found
+          when 303; response.extend CurbFu::Response::SeeOther
+          when 304; response.extend CurbFu::Response::NotModified
+          when 305; response.extend CurbFu::Response::UseProxy
+          when 307; response.extend CurbFu::Response::TemporaryRedirect
           end
-        when 400..499:
+        when 400..499;
           response.extend CurbFu::Response::ClientError
           case response.status
-          when 400: response.extend CurbFu::Response::BadRequest
-          when 401: response.extend CurbFu::Response::Unauthorized
-          when 402: response.extend CurbFu::Response::PaymentRequired
-          when 403: response.extend CurbFu::Response::Forbidden
-          when 404: response.extend CurbFu::Response::NotFound
-          when 405: response.extend CurbFu::Response::MethodNotAllowed
-          when 406: response.extend CurbFu::Response::NotAcceptable
-          when 407: response.extend CurbFu::Response::ProxyAuthenticationRequired
-          when 408: response.extend CurbFu::Response::RequestTimeOut
-          when 409: response.extend CurbFu::Response::Conflict
-          when 410: response.extend CurbFu::Response::Gone
-          when 411: response.extend CurbFu::Response::LengthRequired
-          when 412: response.extend CurbFu::Response::PreconditionFailed
-          when 413: response.extend CurbFu::Response::RequestEntityTooLarge
-          when 414: response.extend CurbFu::Response::RequestURITooLong
-          when 415: response.extend CurbFu::Response::UnsupportedMediaType
-          when 416: response.extend CurbFu::Response::UnsupportedMediaType
-          when 417: response.extend CurbFu::Response::ExpectationFailed
+          when 400; response.extend CurbFu::Response::BadRequest
+          when 401; response.extend CurbFu::Response::Unauthorized
+          when 402; response.extend CurbFu::Response::PaymentRequired
+          when 403; response.extend CurbFu::Response::Forbidden
+          when 404; response.extend CurbFu::Response::NotFound
+          when 405; response.extend CurbFu::Response::MethodNotAllowed
+          when 406; response.extend CurbFu::Response::NotAcceptable
+          when 407; response.extend CurbFu::Response::ProxyAuthenticationRequired
+          when 408; response.extend CurbFu::Response::RequestTimeOut
+          when 409; response.extend CurbFu::Response::Conflict
+          when 410; response.extend CurbFu::Response::Gone
+          when 411; response.extend CurbFu::Response::LengthRequired
+          when 412; response.extend CurbFu::Response::PreconditionFailed
+          when 413; response.extend CurbFu::Response::RequestEntityTooLarge
+          when 414; response.extend CurbFu::Response::RequestURITooLong
+          when 415; response.extend CurbFu::Response::UnsupportedMediaType
+          when 416; response.extend CurbFu::Response::UnsupportedMediaType
+          when 417; response.extend CurbFu::Response::ExpectationFailed
           end
-        when 500..599:
+        when 500..599;
           response.extend CurbFu::Response::ServerError
           case response.status
-          when 500: response.extend CurbFu::Response::InternalServerError
-          when 501: response.extend CurbFu::Response::NotImplemented
-          when 502: response.extend CurbFu::Response::BadGateway
-          when 503: response.extend CurbFu::Response::ServiceUnavailable
-          when 504: response.extend CurbFu::Response::GatewayTimeOut
-          when 505: response.extend CurbFu::Response::VersionNotSupported
+          when 500; response.extend CurbFu::Response::InternalServerError
+          when 501; response.extend CurbFu::Response::NotImplemented
+          when 502; response.extend CurbFu::Response::BadGateway
+          when 503; response.extend CurbFu::Response::ServiceUnavailable
+          when 504; response.extend CurbFu::Response::GatewayTimeOut
+          when 505; response.extend CurbFu::Response::VersionNotSupported
           end
         else
           response.extend CurbFu::Response::UnknownResponse
