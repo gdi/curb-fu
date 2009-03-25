bbc8237f764189c7efdcc13a1d206823fb1bd294
Derek and Matt <drat@gdis-imac-2.local>
2009-03-25 10:40:17 -0400

    Added convenience methods for get/post/put/delete

--- lib/curb-fu.rb
+++ lib/curb-fu.rb
@@ -3,4 +3,20 @@ $:.unshift(dir) unless $:.include?(dir)
 require 'curb-fu/response'
 require 'curb-fu/request'
 
-module CurbFu; end
+module CurbFu
+  def self.get(*args)
+    CurbFu::Request.get(*args)
+  end
+  
+  def self.post(*args)
+    CurbFu::Request.post(*args)
+  end
+  
+  def self.put(*args)
+    CurbFu::Request.put(*args)
+  end
+  
+  def self.delete(*args)
+    CurbFu::Request.delete(*args)
+  end
+end
