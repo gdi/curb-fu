e92355eea9d63aa5e1c886303bff739acec46525
Derek Kastner <dkastner@gmail.com>
2009-03-29 13:28:06 -0400

    merged changes

--- spec/lib/curb-fu/response_spec.rb
+++ spec/lib/curb-fu/response_spec.rb
@@ -2,13 +2,22 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'htmlentities'
 
 describe CurbFu::Response::Base do
-  it "should create a success response" do
-    mock_curb = mock(Object, :response_code => 200, :body_str => 'OK', :header_str => "", :timeout= => nil)
-    r = CurbFu::Response::Base.create(mock_curb)
-    r.should be_a_kind_of(CurbFu::Response::Base)
-    r.should be_a_kind_of(CurbFu::Response::Success)
-    r.should be_a_kind_of(CurbFu::Response::OK)
-    r.should_not be_a_kind_of(CurbFu::Response::Created)
+  describe "successes" do
+    it "should create a success (200) response" do
+      mock_curb = mock(Object, :response_code => 200, :body_str => 'OK')
+      r = CurbFu::Response::Base.create(mock_curb)
+      r.should be_a_kind_of(CurbFu::Response::Base)
+      r.should be_a_kind_of(CurbFu::Response::Success)
+      r.should be_a_kind_of(CurbFu::Response::OK)
+      r.should_not be_a_kind_of(CurbFu::Response::Created)
+    end
+    it "should create a success (201) response" do
+      mock_curb = mock(Object, :response_code => 201, :body_str => 'OK')
+      r = CurbFu::Response::Base.create(mock_curb)
+      r.should be_a_kind_of(CurbFu::Response::Base)
+      r.should be_a_kind_of(CurbFu::Response::Success)
+      r.should be_a_kind_of(CurbFu::Response::Created)
+    end
   end
   it "should create a 400 response" do
     mock_curb = mock(Object, :response_code => 404, :body_str => 'OK', :header_str => "", :timeout= => nil)
